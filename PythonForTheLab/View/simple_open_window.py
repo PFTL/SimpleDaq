889286887d8032363b4cdffc1093cf9971532e3f
Aquiles Carattino <aquiles@aquicarattino.com>
2018-02-01 18:48:21 +0100

    Updated the simple_open_window according to text

--- PythonForTheLab/View/simple_open_window.py
+++ PythonForTheLab/View/simple_open_window.py
@@ -15,15 +15,18 @@ class MainWindow(QtGui.QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
 
-        p = os.path.dirname(__file__)
-        uic.loadUi(os.path.join(p, 'GUI/main_window.ui'), self)
+#        p = os.path.dirname(__file__)
+#        uic.loadUi(os.path.join(p, 'GUI/main_window.ui'), self)
+        uic.loadUi('GUI/main_window.ui', self)
         self.startButton.clicked.connect(self.clicked)
 
     def clicked(self):
         print('Clicked')
 
 if __name__ == '__main__':
+    print(os.getcwd())
     app = QApplication(sys.argv)
     m = MainWindow()
     m.show()
     app.exit(app.exec_())
+
