90489261dc946c8cd2fb03b55dd77078b307c1f1
Aquiles Carattino <aquiles@aquicarattino.com>
2018-04-05 13:43:40 +0200

    Improved experiment model

--- PythonForTheLab/Model/experiment/IV_measurement.py
+++ PythonForTheLab/Model/experiment/IV_measurement.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 """
 Experiment Model
 ================
@@ -13,18 +14,20 @@ from time import time, sleep
 from PythonForTheLab import Q_
 
 
-class Experiment():
+class Experiment:
     """Class for performing a measurement of an I-V curve of a light emitting photodiode (LED).
     """
+
     def __init__(self):
         self.daq = None
         self.properties = {}
         self.stop_monitor = False
-        self.xdata = np.zeros((0))
-        self.ydata = np.zeros((0))
-        self.xdata_scan = np.zeros((0))
-        self.ydata_scan = np.zeros((0))
+        self.xdata = np.zeros(0)
+        self.ydata = np.zeros(0)
+        self.xdata_scan = np.zeros(0)
+        self.ydata_scan = np.zeros(0)
         self.running_scan = False
+        self.delta_x = 0
 
     def read_analog(self, port):
         """Re-implements the function as provided by the model.
@@ -36,6 +39,8 @@ class Experiment():
         return value
 
     def read_digital(self, port):
+        """Reads the value of a digital port.
+        """
         return self.daq.get_digital_value(port)
 
     def do_scan(self):
@@ -43,26 +48,26 @@ class Experiment():
         it relies on having the proper properties set in the dictionary properties['Scan']
         """
         self.running_scan = True
-        start = self.properties['Scan']['start']
-        stop = self.properties['Scan']['stop']
-        step = self.properties['Scan']['step']
+        start = Q_(self.properties['Scan']['start'])
+        stop = Q_(self.properties['Scan']['stop'])
+        step = Q_(self.properties['Scan']['step'])
         port_in = self.properties['Scan']['port_in']
         port_out = self.properties['Scan']['port_out']
-        delay = self.properties['Scan']['delay']
+        delay = Q_(self.properties['Scan']['delay'])
         self.stop_scan = False
 
         units = start.u
         stop = stop.to(units)
-        num_points = (stop-start)/step
+        num_points = (stop - start) / step
         num_points = int(num_points.m_as(''))
-        scan = np.linspace(start, stop, num_points+1)
+        scan = np.linspace(start, stop, num_points + 1)
         self.xdata_scan = scan
-        self.ydata_scan = np.zeros((num_points+1))
+        self.ydata_scan = np.zeros((num_points + 1))
         i = 0
         for value in scan:
             if self.stop_scan:
                 break
-            value = value*units
+            value = value * units
             self.daq.set_analog_value(port_out, value)
             sleep(delay.m_as('s'))
             data = self.read_analog(port_in).m_as('V')
@@ -76,15 +81,15 @@ class Experiment():
         """
         delay = self.properties['Monitor']['time_resolution']
         total_time = self.properties['Monitor']['total_time'].m_as('s')
-        self.xdata = np.zeros((int(total_time/delay.m_as('s'))))
+        self.xdata = np.zeros((int(total_time / delay.m_as('s'))))
         self.delta_x = delay.m_as('s')
-        self.ydata = np.zeros(int(total_time/delay.m_as('s')))
+        self.ydata = np.zeros(int(total_time / delay.m_as('s')))
         self.t0 = time()
         while not self.stop_monitor:
             self.ydata = np.roll(self.ydata, -1)
             self.ydata[-1] = self.read_analog(1).m_as('V')
             self.xdata = np.roll(self.xdata, -1)
-            self.xdata[-1] = time()-self.t0 #self.xdata[-2] + self.delta_x
+            self.xdata[-1] = time() - self.t0  # self.xdata[-2] + self.delta_x
             sleep(delay.m_as('s'))
 
     def load_config(self, filename=None):
@@ -100,15 +105,17 @@ class Experiment():
 
         self.properties = d
         self.properties['config_file'] = filename
-        self.properties['Scan']['start'] = Q_(self.properties['Scan']['start'])
-        self.properties['Scan']['stop'] = Q_(self.properties['Scan']['stop'])
-        self.properties['Scan']['step'] = Q_(self.properties['Scan']['step'])
-        self.properties['Scan']['delay'] = Q_(self.properties['Scan']['delay'])
-        self.properties['Scan']['refresh_time'] = Q_(self.properties['Scan']['refresh_time'])
-
-        self.properties['Monitor']['time_resolution'] = Q_(self.properties['Monitor']['time_resolution'])
-        self.properties['Monitor']['refresh_time'] = Q_(self.properties['Monitor']['refresh_time'])
-        self.properties['Monitor']['total_time'] = Q_(self.properties['Monitor']['total_time'])
+        self.properties['User'] = self.properties['User']['name']
+        self.properties = self.properties['Experiment']
+        # self.properties['Scan']['start'] = Q_(self.properties['Scan']['start'])
+        # self.properties['Scan']['stop'] = Q_(self.properties['Scan']['stop'])
+        # self.properties['Scan']['step'] = Q_(self.properties['Scan']['step'])
+        # self.properties['Scan']['delay'] = Q_(self.properties['Scan']['delay'])
+        # self.properties['Scan']['refresh_time'] = Q_(self.properties['Scan']['refresh_time'])
+        #
+        # self.properties['Monitor']['time_resolution'] = Q_(self.properties['Monitor']['time_resolution'])
+        # self.properties['Monitor']['refresh_time'] = Q_(self.properties['Monitor']['refresh_time'])
+        # self.properties['Monitor']['total_time'] = Q_(self.properties['Monitor']['total_time'])
 
     def load_devices(self, filename=None):
         """Load devices is not used in the Python for the Lab book, but is a handy procedure to work with
@@ -163,13 +170,14 @@ class Experiment():
         """
         if daq_model is None:
             if 'DAQ' in self.properties:
-                if self.properties['DAQ']['name'] == 'DummyDaq':
+                name = self.properties['DAQ']['name']
+                port = self.properties['DAQ']['port']
+                if name == 'DummyDaq':
                     from PythonForTheLab.Model.daq import DummyDaq
-                    self.daq = DummyDaq()
+                    self.daq = DummyDaq(port)
 
-                elif self.properties['DAQ']['name'] == 'RealDaq':
+                elif name == 'RealDaq':
                     from PythonForTheLab.Model.daq import AnalogDaq
-                    port = self.properties['DAQ']['port']
                     self.daq = AnalogDaq(port)
 
                 else:
@@ -189,7 +197,7 @@ class Experiment():
         letters).
         """
         i = 0
-        ext = file_path[-4:] # Get the file extension (it assumes is a dot and three letters)
+        ext = file_path[-4:]  # Get the file extension (it assumes is a dot and three letters)
         filename = file_path[:-4]
         while os.path.exists(file_path):
             file_path = filename + '_' + str(i) + ext
