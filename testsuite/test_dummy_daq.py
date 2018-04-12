4ef84fae88bbd2553fb8e6888baa75f2cd497aa7
Aquiles Carattino <aquiles@aquicarattino.com>
2018-04-12 14:26:12 +0200

    Started to improve test suite

--- testsuite/test_dummy_daq.py
+++ testsuite/test_dummy_daq.py
@@ -3,13 +3,18 @@
 import unittest
 from PythonForTheLab.Model.daq import DummyDaq
 
-dummy_model = DummyDaq(0)
 
 class DummyDaqTest(unittest.TestCase):
-
+    def setUp(self):
+        self.dummy_model = DummyDaq(0)
     def test_analog_output(self):
-        dummy_model.set_analog_value(0, 10)
+        self.dummy_model.set_analog_value(0, 10)
 
     def test_analog_input(self):
-        value = dummy_model.get_analog_value(0)
+        value = self.dummy_model.get_analog_value(0)
         self.assertGreaterEqual(value.m, 0, 'The output of the reading should be >0 ')
+
+    def test_idn(self):
+        idn = self.dummy_model.idn()
+        if not isinstance(idn, str):
+            self.fail('Failed to get the IDN')
